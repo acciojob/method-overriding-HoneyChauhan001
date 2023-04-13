6a7683acbd33294d8bec56aeb2c7dbfd7a39d2de
Honey Chauhan <hc924508@gmail.com>
2023-04-13 19:15:20 +0530

    Overridden method | public method

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,14 @@
 package com.driver.test;
 
 import static org.junit.Assert.*;
+
+import com.driver.B;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
+    public static void main(String[] args) {
+        com.driver.B b = new B();
+        b.meth();
+    }
     
 }
