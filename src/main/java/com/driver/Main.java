e5f13989b57e8e0889399adb1c861c116c2bda47
Honey Chauhan <hc924508@gmail.com>
2023-04-15 03:21:53 +0530

    class A & B in Main class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,24 @@
 package com.driver;
 
 public class Main {
+
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+        @Override
+        public String meth(){
+            return "Method is overridden in Extended class B";
+        }
+    }
     public static void main(String[] args) {
-        classB b = new classB();
-        b.meth();
+
+        B obj = new B();
+
+        obj.meth();
     }
 
 }
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,7 +1,4 @@
 package com.driver.test;
 
-import com.driver.classB;
-import com.driver.classA;
-
 public class TestCases {
 }
